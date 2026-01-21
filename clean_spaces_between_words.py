2e910c36fbc716e6e9f3851a31abf26f55ff3938
prasadmhase14 <mhaseprasad@gmail.com>
2026-01-21 20:22:34 +0530

    3rd commit to new brach

---
+++ clean_spaces_between_words.py
@@ -0,0 +1,6 @@
+# 9. Clean Multiple Spaces Between Words
+# Problem: Remove extra spaces between words.
+# sentence = "Python    is   easy"
+# clean_sentence = ' '.join(sentence.split())
+# print("Cleaned:", clean_sentence)
+
